3e628d606dbe402507cb9a43ed16294e64acae35
Isaac Seymour <i.seymour@oxon.org>
2019-10-17 19:20:54 +0100

    remove unused HTTP client config

--- src/crawler.go
+++ src/crawler.go
@@ -3,17 +3,10 @@ package crawler
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func Crawl(baseUrl string) (fmt.Stringer, error) {
-	client := &http.Client{
-		Transport: &http.Transport{
-			MaxIdleConns:       10,
-			IdleConnTimeout:    30 * time.Second,
-			DisableCompression: true,
-		},
-	}
+	client := &http.Client{}
 	c := &crawler{client: client, sitemap: NewSitemap()}
 	sitemap, err := c.Run(baseUrl)
 	return sitemap, err
